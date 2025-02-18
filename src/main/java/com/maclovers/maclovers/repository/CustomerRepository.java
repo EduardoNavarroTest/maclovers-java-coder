a428ed70c23c7ee3ae8adc1eff1889ca8d5536eb
Eduardo Enrique Navarro Echeverria <eenavarro@sprc.com.co>
2025-02-18 08:00:32 -0500

    Se añade anotación de Repository

--- src/main/java/com/maclovers/maclovers/repository/CustomerRepository.java
+++ src/main/java/com/maclovers/maclovers/repository/CustomerRepository.java
@@ -1,8 +1,11 @@
 package com.maclovers.maclovers.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.maclovers.maclovers.entity.Customer;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 }
--- src/main/java/com/maclovers/maclovers/repository/OrderRepository.java
+++ src/main/java/com/maclovers/maclovers/repository/OrderRepository.java
@@ -1,8 +1,11 @@
 package com.maclovers.maclovers.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.maclovers.maclovers.entity.Order;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
